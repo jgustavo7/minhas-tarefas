ddf70abfb9b6c8250ea7a5bd8d63e83dc6109bf0
jgustavo <jgustavo0711@gmail.com>
2024-12-02 15:56:29 -0300

    projeto finalizado

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,20 +1,67 @@
 import { useSelector } from 'react-redux'
 
 import Tarefa from '../../components/Tarefa'
-import { Container } from './styles'
-import * as enums from '../../utils/enums/Tarefa'
+import { MainContainer, Titulo } from '../../styles'
 
 import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state)
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo, criterio, valor } = useSelector(
+    (state: RootReducer) => state.filtro
+  )
+
+  const filtraTarefas = () => {
+    let tarefasFiltradas = itens
+    if (termo !== undefined) {
+      tarefasFiltradas = tarefasFiltradas.filter(
+        (item) => item.titulo.toLowerCase().search(termo.toLowerCase()) >= 0
+      )
+
+      if (criterio === 'prioridade') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.prioridade === valor
+        )
+      } else if (criterio === 'status') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.status === valor
+        )
+      }
+
+      return tarefasFiltradas
+    } else {
+      return itens
+    }
+  }
+
+  const exibeResultadoFiltragem = (quantidade: number) => {
+    let mensagem = ''
+
+    const complementacao =
+      termo !== undefined && termo.length > 0 ? `e "${termo}"` : ''
+
+    if (criterio === 'todas') {
+      mensagem = `${quantidade} tarefa(s) encontrada(s) como: todas ${complementacao}`
+    } else {
+      mensagem = `${quantidade} tarefa(s) encontrada(s) como: "${`${criterio}=${valor}`}" ${complementacao}`
+    }
+
+    return mensagem
+  }
+
+  const tarefas = filtraTarefas()
+  const mensagem = exibeResultadoFiltragem(tarefas.length)
 
   return (
-    <Container>
+    <MainContainer>
+      <ul>
+        <Titulo as="p">{mensagem}</Titulo>
+      </ul>
       <ul>
         {tarefas.map((t) => (
           <li key={t.titulo}>
             <Tarefa
+              id={t.id}
               descricao={t.descricao}
               titulo={t.titulo}
               status={t.status}
@@ -23,7 +70,7 @@ const ListaDeTarefas = () => {
           </li>
         ))}
       </ul>
-    </Container>
+    </MainContainer>
   )
 }
 
--- src/styles/variaveis.ts
+++ src/styles/variaveis.ts
@@ -2,5 +2,6 @@ export default {
   vermelho: '#C23616',
   verde: '#44BD32',
   amarelo: '#f0C11A',
-  amarelo2: '#E1a32A'
+  amarelo2: '#E1a32A',
+  azulEscuro: '#2f3640'
 }
